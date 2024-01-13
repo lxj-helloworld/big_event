140d0edf279521cf9533a87aff00fc409d0c325f
xiaojinli <xiaojin20135@live.com>
2024-01-13 22:27:02 +0800

    :feat:使用Validated对参数进行校验，捕获全局异常

--- src/main/java/com/xiaojin20135/example/big_event/controller/UserController.java
+++ src/main/java/com/xiaojin20135/example/big_event/controller/UserController.java
@@ -3,20 +3,23 @@ package com.xiaojin20135.example.big_event.controller;
 import com.xiaojin20135.example.big_event.pojo.Result;
 import com.xiaojin20135.example.big_event.pojo.User;
 import com.xiaojin20135.example.big_event.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result regidter(String username, String password) {
+    public Result regidter(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
         //查询用户
         User user = userService.findByUserName(username);
         if (null == user) {
